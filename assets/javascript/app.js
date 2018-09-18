cf8b8f1d9653e422abdcb77e5eb428c393223453
trwylde <trwylde@gmail.com>
2018-09-18 16:44:34 -0400

    Building html.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC4302VzuzWrzMYxqvkgZITb4AulC_mmQQ",
+  authDomain: "train-scheduler-1c977.firebaseapp.com",
+  databaseURL: "https://train-scheduler-1c977.firebaseio.com",
+  projectId: "train-scheduler-1c977",
+  storageBucket: "train-scheduler-1c977.appspot.com",
+  messagingSenderId: "1007731290219"
+};
+firebase.initializeApp(config);
